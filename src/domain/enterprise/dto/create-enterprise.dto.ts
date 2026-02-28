d1669fd338363ec77eafadeb9d6dfbf35ef19a4f
Necobgs <emanuelborgesdev@gmail.com>
2026-02-28 18:11:08 -0300

    feat: empresa validado

--- src/domain/enterprise/dto/create-enterprise.dto.ts
+++ src/domain/enterprise/dto/create-enterprise.dto.ts
@@ -1,17 +1,21 @@
 import { IsString, IsBoolean, IsOptional, Length } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class CreateEnterpriseDto {
 	@ApiProperty({ example: 'Acme Ltda' })
 	@IsString()
+	@Transform(({ value }) => value.toUpperCase().trim())
 	name!: string;
 
 	@ApiProperty({ example: '12345678000199' })
 	@IsString()
+	@Transform(({ value }) => value.trim())
 	@Length(14, 18)
 	cnpj!: string;
 
 	@ApiProperty({ example: '+551130000000' })
+	@Transform(({ value }) => value.trim())
 	@IsString()
 	phone!: string;
 }
